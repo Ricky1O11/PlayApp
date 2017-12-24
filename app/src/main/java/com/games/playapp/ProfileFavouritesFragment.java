5a7bd3bb8573c626d5f328f8b944d1dd8f3ea64a
Christian Quintavalle <quintavalle.christian@gmail.com>
2017-12-24 19:04:14 +0100

    Add favorites in profile and handle visualization bugs

--- app/src/main/java/com/games/playapp/ProfileFavouritesFragment.java
+++ app/src/main/java/com/games/playapp/ProfileFavouritesFragment.java
@@ -2,30 +2,56 @@ package com.games.playapp;
 
 import android.os.Bundle;
 import android.support.v4.app.Fragment;
+import android.support.v7.widget.GridLayoutManager;
+import android.support.v7.widget.RecyclerView;
+import android.util.Log;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
-
-import com.google.firebase.database.DataSnapshot;
-import com.google.firebase.database.DatabaseError;
+import java.util.LinkedList;
 import java.util.Map;
 
-/**
- * Created by benjy on 16/12/2017.
- */
 
-public class ProfileFavouritesFragment extends Fragment {
-    public static Map<String, Object> favourites;
+public class ProfileFavouritesFragment extends Fragment implements BoardgamesFavouriteAdapter.BoardgamesFavouriteAdapterOnClickHandler{
+    public static BoardgamesFavouriteAdapter mBoardgamesAdapter;
+    private RecyclerView mRecycler;
+
+
+    private LinkedList<BgModel> boardgames = new LinkedList();
+
+    @Override
     public View onCreateView(LayoutInflater inflater, ViewGroup container,
                              Bundle savedInstanceState) {
-        // Inflate the layout for this fragment
-        return inflater.inflate(R.layout.fragment_profile_favourites, container, false);
-    }
-    public static Map<String,Object> getFavourites(){
-        return favourites;
+        View view =  inflater.inflate(R.layout.fragment_profile_favourites, container, false);
+
+        mRecycler = (RecyclerView) view.findViewById(R.id.recyclerview_boardgame_favourites);
+        mBoardgamesAdapter = new BoardgamesFavouriteAdapter(getContext(), this);
+
+        mRecycler.setAdapter(mBoardgamesAdapter);
+        GridLayoutManager gridLayoutManager = new GridLayoutManager(getContext(), 4);
+        mRecycler.setLayoutManager(gridLayoutManager);
+        mRecycler.setHasFixedSize(true);
+
+        boardgames.clear();
+        for (Map.Entry<String, Object> game : SignedInActivity.favourites.entrySet()) {
+            BgModel boardgame = new BgModel((Map) game.getValue());
+            boardgames.addFirst(boardgame);
+        }
+        mBoardgamesAdapter.swapArrayList(boardgames);
+
+        return view;
     }
 
+    @Override
+    public void onClick(String id) {
+        BoardgameDetailFragment bgdet= new BoardgameDetailFragment();
+        Bundle bundle = new Bundle();
+        bundle.putString("BG_KEY", id);
+        bgdet.setArguments(bundle);
+
+        getActivity().getSupportFragmentManager().beginTransaction()
+                .replace(R.id.fragment_container, bgdet,null)
+                .addToBackStack(null)
+                .commit();
+    }
 }
-
-
-
